ff43724cb19fc1897ddeca8ef0f8fc899b142db1
Penelope <pennyblack.p@gmail.com>
2019-06-18 22:48:41 +0200

    js adjustments

--- script.js
+++ script.js
@@ -4,79 +4,53 @@ console.log("starting fetch call");
 const photosPromise = fetch('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=fbd58be6b4f585662024b8f54c2d74f4&gallery_id=66911286-72157651066599621&format=json&nojsoncallback=1')
 console.log("after fetch call");
 console.log(photosPromise);
-console.log("after it has run");
+console.log("end");
 
 photosPromise
 .then(data => data.json())
 .then(data => {
     data.photos.photo.forEach((object) => {
         console.log(object);
+        createImages(object);
     });
     console.log(data);
 })
 
-
-
-function createImages(images) {
-    for (let i of imgs) {
-        const image = document.createElement('img');
-        image.setAttribute('photos', i.dowload_url);
-        image.classList.add('loading');
-        document.getElementById(div).appendChild(image);
+function createImages(image) {
+    var img = document.createElement('img');
+    var url = 'https://farm' + image.farm + '.staticflickr.com/' + image.server + '/' + image.id + '_' + image.secret + '.jpg';
+    img.setAttribute('src', url);
+    //console.log(image);
+   // var img = document.createElement('img');
+    const elem = document.getElementById('flex-container');
+    console.log(elem);
+    elem.appendChild(img);
     }
-}
+/*functioning code
+function createImages(image) {
+    const url = 'https://farm' + image.farm + '.staticflickr.com/' + image.server + '/' + image.id + '_' + image.secret + '.jpg';
+  //console.log(url);
+    const img = document.createElement("img");
+    img.setAttribute('src', url);
+  //document.appendChild(img);
+    const elem = document.querySelector('#flex-container');
+  //console.log(elem);
+    elem.appendChild(img);
+    //for (let i of data) {
+        /*const object = document.createElement("img");
+        
+        object.setAttribute('src', url);
+        document.appendChild(img);
+    //}*/
 
-//the code above results to the json. ES6
-
-
-//the code below is alternatives that supposedly could load the pictures by creating new elements
-/*document.body.onload = addElement;
-function addElement () {
-    var newDiv = document.createElement('div');
-    var newImage = document.createElement('img');
-    newDiv.appendChild(newImage);
-    var currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
+/*pt 2 functioning rewritten
+function createImages(image) {
+    var img = document.createElement('img');
+    var url = 'https://farm' + image.farm + '.staticflickr.com/' + image.server + '/' + image.id + '_' + image.secret + '.jpg';
+    img.setAttribute('src', url);
+    //console.log(image);
+   // var img = document.createElement('img');
+    const elem = document.getElementById('flex-container');
+    console.log(elem);
+    elem.appendChild(img);
 }*/
-
-/*window.onload = () => {
-    fetch('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=fbd58be6b4f585662024b8f54c2d74f4&gallery_id=66911286-72157651066599621&format=json&nojsoncallback=1')
-        .then(function(response) {
-            return response.json();
-        })
-            .then(function(myJson) {
-                createImages(myJson);
-                document.getElementById('container').append(image)
-                .catch(err => {
-                    console.log(err);
-                });
-            } */
-
-            /*function photosResponse (rsp) {
-
-                if (rsp.stat != "ok") {
-                    return;
-                }
-            
-                for (var i=0; i<rsp.photos.photo.length; i++) {
-            
-                var photo = rsp.photos.photo[i];
-            
-                var div = document.createElement ('div');
-                var img = document.createElement ('img');
-            
-                div.appendChild(img);
-
-                var element = document.getElementById(flex-container);
-                element.appendChild(div);
-                //document.body.appendChild(div);
-                    }
-            }
-
-
-    /*var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" +today.getSeconds();
-    var dateTime = date+' '+time;
-
-    document.body.innerHTML="<p>Date:"+dateTime+"</p>"*/
